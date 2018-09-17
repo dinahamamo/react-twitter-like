497daecb242bf108f8c6f411add766d0eb9a4d12
dinahamamo <dina.hamamo@gmail.com>
2018-09-17 03:58:28 +0200

    React router

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,11 +3,12 @@ import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
   toParent = (e, id) => {
     e.preventDefault()
-    // TODO: Redirect to parent tweet
+    this.props.history.push(`/tweet/${id}`)
   }
 
   handleLike = (e) => {
@@ -28,11 +29,11 @@ class Tweet extends Component {
     }
 
     const {
-      name, avatar, timestamp, text, hasLiked, likes, replies, parent
+      name, avatar, timestamp, text, hasLiked, likes, replies, parent, id
     } = tweet
 
     return (
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img
           src={avatar}
           alt={`Avatar of ${name}`}
@@ -61,7 +62,7 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -78,4 +79,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
